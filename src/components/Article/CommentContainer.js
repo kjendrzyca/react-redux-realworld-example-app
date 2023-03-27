73658bfd50ad2a68b9ccfd66a00920bb9c2ac3b8
kjendrzyca <kjendrzyca@users.noreply.github.com>
2023-03-27 12:17:03 +0200

    Use hooks: ListErrors

--- src/components/Article/CommentContainer.js
+++ src/components/Article/CommentContainer.js
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import CommentInput from './CommentInput';
 import CommentList from './CommentList';
+import ListErrors from '../ListErrors';
 
 function CommentContainer(props) {
   if (props.currentUser) {
     return (
       <div className="col-xs-12 col-md-8 offset-md-2">
         <div>
-          <list-errors errors={props.errors} />
+          <ListErrors errors={props.errors} />
           <CommentInput slug={props.slug} currentUser={props.currentUser} />
         </div>
 
