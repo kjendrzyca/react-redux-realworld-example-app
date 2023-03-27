0791116f14452d1554d6534420e418a9de214346
kjendrzyca <kjendrzyca@users.noreply.github.com>
2023-03-27 11:49:44 +0200

    Use hooks: Article

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import marked from 'marked';
 import ArticleMeta from './ArticleMeta';
@@ -21,80 +21,80 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch({ type: ARTICLE_PAGE_UNLOADED }),
 });
 
-class Article extends React.Component {
-  componentWillMount() {
-    this.props.onLoad(Promise.all([
-      agent.Articles.get(this.props.match.params.id),
-      agent.Comments.forArticle(this.props.match.params.id),
+function Article(props) {
+  useEffect(() => {
+    props.onLoad(Promise.all([
+      agent.Articles.get(props.match.params.id),
+      agent.Comments.forArticle(props.match.params.id),
     ]));
+
+    return () => props.onUnload();
+    // 💡 hint: destructure props and update dependencies
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!props.article) {
+    return null;
   }
 
-  componentWillUnmount() {
-    this.props.onUnload();
-  }
+  const markup = { __html: marked(props.article.body, { sanitize: true }) };
 
-  render() {
-    if (!this.props.article) {
-      return null;
-    }
+  const canModify = props.currentUser
+    && props.currentUser.username === props.article.author.username;
 
-    const markup = { __html: marked(this.props.article.body, { sanitize: true }) };
-    const canModify = this.props.currentUser
-      && this.props.currentUser.username === this.props.article.author.username;
-    return (
-      <div className="article-page">
+  return (
+    <div className="article-page">
 
-        <div className="banner">
-          <div className="container">
+      <div className="banner">
+        <div className="container">
 
-            <h1>{this.props.article.title}</h1>
-            <ArticleMeta
-              article={this.props.article}
-              canModify={canModify}
-            />
+          <h1>{props.article.title}</h1>
+          <ArticleMeta
+            article={props.article}
+            canModify={canModify}
+          />
 
-          </div>
-        </div>
-
-        <div className="container page">
-
-          <div className="row article-content">
-            <div className="col-xs-12">
-
-              <div dangerouslySetInnerHTML={markup} />
-
-              <ul className="tag-list">
-                {
-                  this.props.article.tagList.map((tag) => (
-                    <li
-                      className="tag-default tag-pill tag-outline"
-                      key={tag}
-                    >
-                      {tag}
-                    </li>
-                  ))
-                }
-              </ul>
-
-            </div>
-          </div>
-
-          <hr />
-
-          <div className="article-actions" />
-
-          <div className="row">
-            <CommentContainer
-              comments={this.props.comments || []}
-              errors={this.props.commentErrors}
-              slug={this.props.match.params.id}
-              currentUser={this.props.currentUser}
-            />
-          </div>
         </div>
       </div>
-    );
-  }
+
+      <div className="container page">
+
+        <div className="row article-content">
+          <div className="col-xs-12">
+
+            <div dangerouslySetInnerHTML={markup} />
+
+            <ul className="tag-list">
+              {
+                props.article.tagList.map((tag) => (
+                  <li
+                    className="tag-default tag-pill tag-outline"
+                    key={tag}
+                  >
+                    {tag}
+                  </li>
+                ))
+              }
+            </ul>
+
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="article-actions" />
+
+        <div className="row">
+          <CommentContainer
+            comments={props.comments || []}
+            errors={props.commentErrors}
+            slug={props.match.params.id}
+            currentUser={props.currentUser}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Article);
