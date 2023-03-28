cb18ba8f85ba88367aa5d6593e119636683c6689
kjendrzyca <kjendrzyca@users.noreply.github.com>
2023-03-28 13:05:23 +0200

    Use redux hooks: DeleteButton

--- src/components/Article/DeleteButton.js
+++ src/components/Article/DeleteButton.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import agent from '../../agent';
 import { DELETE_COMMENT } from '../../constants/actionTypes';
 
-const mapDispatchToProps = (dispatch) => ({
-  onClick: (payload, commentId) => dispatch({ type: DELETE_COMMENT, payload, commentId }),
-});
-
 function DeleteButton(props) {
+  const dispatch = useDispatch();
+  const onClick = (payload, commentId) => dispatch({ type: DELETE_COMMENT, payload, commentId });
   const del = () => {
     const payload = agent.Comments.delete(props.slug, props.commentId);
-    props.onClick(payload, props.commentId);
+    onClick(payload, props.commentId);
   };
 
   if (props.show) {
@@ -24,4 +22,4 @@ function DeleteButton(props) {
   return null;
 }
 
-export default connect(() => ({}), mapDispatchToProps)(DeleteButton);
+export default DeleteButton;
