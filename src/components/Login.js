0be6d6eec5b0bc060f2949e86712baec021df1ff
kjendrzyca <kjendrzyca@users.noreply.github.com>
2023-03-28 13:58:05 +0200

    Use redux hooks: Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import ListErrors from './ListErrors';
 import agent from '../agent';
 import {
@@ -9,47 +9,39 @@ import {
   LOGIN_PAGE_UNLOADED,
 } from '../constants/actionTypes';
 
-const mapStateToProps = (state) => ({ ...state.auth });
+function Login() {
+  const dispatch = useDispatch();
 
-const mapDispatchToProps = (dispatch) => ({
-  onChangeEmail: (value) => dispatch({ type: UPDATE_FIELD_AUTH, key: 'email', value }),
-  onChangePassword: (value) => dispatch({ type: UPDATE_FIELD_AUTH, key: 'password', value }),
-  // eslint-disable-next-line max-len
-  onSubmit: (email, password) => dispatch({ type: LOGIN, payload: agent.Auth.login(email, password) }),
-  onUnload: () => dispatch({ type: LOGIN_PAGE_UNLOADED }),
-});
+  const email = useSelector((state) => state.auth.email);
+  const password = useSelector((state) => state.auth.password);
+  const errors = useSelector((state) => state.auth.errors);
+  const inProgress = useSelector((state) => state.auth.inProgress);
 
-function Login(props) {
-  const changeEmail = (ev) => props.onChangeEmail(ev.target.value);
-  const changePassword = (ev) => props.onChangePassword(ev.target.value);
+  const changeEmail = (ev) => dispatch({ type: UPDATE_FIELD_AUTH, key: 'email', value: ev.target.value });
+  const changePassword = (ev) => dispatch({ type: UPDATE_FIELD_AUTH, key: 'password', value: ev.target.value });
+
+  // eslint-disable-next-line no-shadow
   const submitForm = (email, password) => (ev) => {
     ev.preventDefault();
-    props.onSubmit(email, password);
+    dispatch({ type: LOGIN, payload: agent.Auth.login(email, password) });
   };
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => () => props.onUnload(), []);
+  useEffect(() => () => dispatch({ type: LOGIN_PAGE_UNLOADED }), [dispatch]);
 
-  const email = props.email;
-  const password = props.password;
   return (
     <div className="auth-page">
       <div className="container page">
         <div className="row">
-
           <div className="col-md-6 offset-md-3 col-xs-12">
             <h1 className="text-xs-center">Sign In</h1>
             <p className="text-xs-center">
-              <Link to="/register">
-                Need an account?
-              </Link>
+              <Link to="/register">Need an account?</Link>
             </p>
 
-            <ListErrors errors={props.errors} />
+            <ListErrors errors={errors} />
 
             <form onSubmit={submitForm(email, password)}>
               <fieldset>
-
                 <fieldset className="form-group">
                   <input
                     className="form-control form-control-lg"
@@ -73,19 +65,17 @@ function Login(props) {
                 <button
                   className="btn btn-lg btn-primary pull-xs-right"
                   type="submit"
-                  disabled={props.inProgress}
+                  disabled={inProgress}
                 >
                   Sign in
                 </button>
-
               </fieldset>
             </form>
           </div>
-
         </div>
       </div>
     </div>
   );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default Login;
